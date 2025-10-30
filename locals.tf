22a2eb8e097e04ed862d393366de1862fc12ba44
Ilya Repnikov <Ilya_Repnikov@epam.com>
2025-10-30 19:51:49 +0400

    fmt

--- locals.tf
+++ locals.tf
@@ -4,7 +4,7 @@ locals {
   alb_name             = "cmtr-fvj3554p-loadbalancer"
 
   # Ensure subnets are in different Availability Zones
-  public_subnets = distinct([for subnet in data.aws_subnets.public_subnets.ids : subnet])
+  public_subnets  = distinct([for subnet in data.aws_subnets.public_subnets.ids : subnet])
   private_subnets = distinct([for subnet in data.aws_subnets.private_subnets.ids : subnet])
 
   ec2_security_group_id           = data.aws_security_group.ec2_sg.id
