4e0f56a98e790235c1b5f165980b1941b165c778
Ilya Repnikov <Ilya_Repnikov@epam.com>
2025-10-30 14:23:44 +0400

    name tag

--- ec2.tf
+++ ec2.tf
@@ -24,6 +24,7 @@ resource "aws_instance" "ec2" {
   vpc_security_group_ids = [aws_security_group.main.id]
 
   tags = {
+    Name    = "${var.id_tag}-ec2"
     Project = var.project_tag
     ID      = var.id_tag
   }
