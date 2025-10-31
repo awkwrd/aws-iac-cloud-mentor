27a371f81aa1c15ad91e020e0f00d87053cabe56
Ilya Repnikov <Ilya_Repnikov@epam.com>
2025-10-31 18:50:40 +0400

    iam-policy import

--- providers.tf
+++ providers.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "cmtr-fvj3554p-backend-new-bucket-1761917529"
+    bucket = "cmtr-fvj3554p-backend-bucket-1761920706"
     key    = "tf_code.tfstate"
     region = "us-east-1"
   }
--- resources.tf
+++ resources.tf
@@ -1,12 +1,15 @@
-resource "aws_iam_policy" "custom_policy" {
-  name        = "resource-move-demo-policy"
+resource "aws_iam_policy" "cmtr_fvj3554p_iam_policy" {
+  name        = "cmtr-fvj3554p-iam-policy"
   description = "Custom role with limited permissions"
-  policy = jsonencode({
+  policy      = jsonencode({
     Version = "2012-10-17",
     Statement = [
       {
         Effect   = "Allow",
-        Action   = ["ec2:*", "s3:*"], # Replace with specific actions
+        Action   = [
+          "ec2:*",
+          "s3:*"
+        ],
         Resource = "*"
       }
     ]
