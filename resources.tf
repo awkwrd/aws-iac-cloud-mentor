e0bbeaa02965081b1265a4c6bb5347ba331ee13d
Ilya Repnikov <Ilya_Repnikov@epam.com>
2025-10-31 18:52:20 +0400

    fmt

--- resources.tf
+++ resources.tf
@@ -1,12 +1,12 @@
 resource "aws_iam_policy" "cmtr_fvj3554p_iam_policy" {
   name        = "cmtr-fvj3554p-iam-policy"
   description = "Custom role with limited permissions"
-  policy      = jsonencode({
+  policy = jsonencode({
     Version = "2012-10-17",
     Statement = [
       {
-        Effect   = "Allow",
-        Action   = [
+        Effect = "Allow",
+        Action = [
           "ec2:*",
           "s3:*"
         ],
