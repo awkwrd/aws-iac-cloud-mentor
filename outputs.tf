0fc3fa763f948454b1819b401aac4ad18e339db5
Ilya Repnikov <Ilya_Repnikov@epam.com>
2025-10-31 12:39:18 +0400

    validation shunt

--- outputs.tf
+++ outputs.tf
@@ -12,3 +12,8 @@ output "security_group_id" {
   description = "The ID of the discovered security group"
   value       = data.aws_security_group.selected.id
 }
+
+output "instance_security_groups" {
+  description = "The security groups attached to the EC2 instance"
+  value       = aws_instance.cmtr_fvj3554p_instance.vpc_security_group_ids
+}
