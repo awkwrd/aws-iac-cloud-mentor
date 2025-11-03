2af6029f438ea0bc15974f72aa12276ad8cfba57
Ilya Repnikov <Ilya_Repnikov@epam.com>
2025-11-03 17:55:23 +0400

    vars def

--- variables.tf
+++ variables.tf
@@ -1,11 +1,13 @@
 variable "region" {
   description = "The AWS region to deploy resources in."
   type        = string
+  default     = "us-east-1"
 }
 
 variable "vpc_id" {
   description = "The ID of the pre-provisioned VPC."
   type        = string
+  default     = "vpc-07dca5b53ee29ccbf"
 }
 
 variable "blue_weight" {
@@ -21,14 +23,17 @@ variable "green_weight" {
 variable "blue_instance_type" {
   description = "The instance type for the Blue environment."
   type        = string
+  default     = "t2.micro"
 }
 
 variable "green_instance_type" {
   description = "The instance type for the Green environment."
   type        = string
+  default     = "t2.micro"
 }
 
 variable "project_name" {
   description = "The name of the project used for tagging and naming resources."
   type        = string
+  default     = "cmtr-fvj3554p"
 }
