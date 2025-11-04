0442adebe1393106b8f3f7192f6e2c757f12048c
Ilya Repnikov <Ilya_Repnikov@epam.com>
2025-11-04 16:26:11 +0400

    fmt

--- main.tf
+++ main.tf
@@ -22,14 +22,14 @@ module "network_security" {
 module "application" {
   source             = "./modules/application"
   subnet_ids         = module.network.public_subnet_ids
-  security_group_ids = [module.network_security.ssh_sg_id, module.network_security.public_http_sg_id]
+  security_group_ids = [module.network_security.ssh_sg_id, module.network_security.private_http_sg_id]
+  public_http_sg_id  = module.network_security.public_http_sg_id
   instance_type      = var.instance_type
   desired_capacity   = var.desired_capacity
   min_size           = var.min_size
   max_size           = var.max_size
   name_prefix        = var.name_prefix
   vpc_id             = module.network.vpc_id # Pass the VPC ID here
-  public_http_sg_id  = module.network_security.public_http_sg_id
   instance_profile   = aws_iam_instance_profile.ssm_instance_profile.name # Pass the instance profile
 
 }
